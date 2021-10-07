cb04282b206bc1a926ab6e37b4cd67450e2c4766
Gabor Boros <gabor.brs@gmail.com>
2021-10-07 17:22:34 +0200

    feat(clockify): add basic clockify client implementation

---
+++ internal/pkg/client/clockify/clockify.go
@@ -0,0 +1,235 @@
+package clockify
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"math"
+	"net/http"
+	"net/url"
+	"regexp"
+	"time"
+
+	"strconv"
+
+	"github.com/gabor-boros/minutes/internal/pkg/client"
+	"github.com/gabor-boros/minutes/internal/pkg/worklog"
+)
+
+const (
+	// DateFormat is the specific format used by Clockify to parse time.
+	DateFormat string = "2006-01-02T15:04:05Z"
+	// MaxPageLength is the maximum page length defined by Clockify.
+	MaxPageLength int = 5000
+	// PathWorklog is the API endpoint used to search and create worklogs.
+	PathWorklog string = "/api/v1/workspaces/%s/user/%s/time-entries"
+)
+
+// Project represents the project assigned to an entry.
+type Project struct {
+	ID         string `json:"id"`
+	Name       string `json:"name"`
+	ClientID   string `json:"clientId"`
+	ClientName string `json:"clientName"`
+}
+
+// Tag represents a tag assigned to an entry.
+type Tag struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// Task represents the task assigned to an entry.
+type Task struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// Interval represents the Start and End date of an entry.
+type Interval struct {
+	Start time.Time `json:"start"`
+	End   time.Time `json:"end"`
+}
+
+// FetchEntry represents the entry fetched from Clockify.
+type FetchEntry struct {
+	Description  string   `json:"description"`
+	Billable     bool     `json:"billable"`
+	Project      Project  `json:"project"`
+	TimeInterval Interval `json:"timeInterval"`
+	Task         Task     `json:"task"`
+	Tags         []Tag    `json:"tags"`
+}
+
+// WorklogSearchParams represents the parameters used to filter search results.
+// Hydrated indicates to return the "expanded" search result. Expanded result
+// contains the project, task, and tag details, not just their ID.
+type WorklogSearchParams struct {
+	Start      string
+	End        string
+	Page       int
+	PageSize   int
+	Hydrated   bool
+	InProgress bool
+}
+
+// ClientOpts is the client specific options, extending client.BaseClientOpts.
+type ClientOpts struct {
+	client.BaseClientOpts
+	Workspace string
+}
+
+type clockifyClient struct {
+	opts *ClientOpts
+}
+
+func (c clockifyClient) getSearchURL(user string, params *WorklogSearchParams) (string, error) {
+	searchPath := fmt.Sprintf(PathWorklog, c.opts.Workspace, user)
+	worklogURL, err := url.Parse(c.opts.BaseURL + searchPath)
+	if err != nil {
+		return "", err
+	}
+
+	queryParams := worklogURL.Query()
+	queryParams.Add("start", params.Start)
+	queryParams.Add("end", params.End)
+	queryParams.Add("page", strconv.Itoa(params.Page))
+	queryParams.Add("page-size", strconv.Itoa(params.PageSize))
+	queryParams.Add("hydrated", strconv.FormatBool(params.Hydrated))
+	queryParams.Add("in-progress", strconv.FormatBool(params.InProgress))
+	worklogURL.RawQuery = queryParams.Encode()
+
+	return fmt.Sprintf("%s?%s", worklogURL.Path, worklogURL.Query().Encode()), nil
+}
+
+func (c clockifyClient) splitEntry(entry FetchEntry, bd time.Duration, ubd time.Duration) (*[]worklog.Entry, error) {
+	r, err := regexp.Compile(c.opts.TasksAsTagsRegex)
+	if err != nil {
+		return nil, err
+	}
+
+	tasks := map[string]string{}
+	for _, tag := range entry.Tags {
+		if task := r.FindString(tag.Name); task != "" {
+			tasks[tag.ID] = task
+		}
+	}
+
+	var items []worklog.Entry
+	totalTasks := len(tasks)
+
+	for taskID, taskName := range tasks {
+		splitBillableDuration := time.Duration(math.Round(float64(bd.Nanoseconds()) / float64(totalTasks)))
+		splitUnbillableDuration := time.Duration(math.Round(float64(ubd.Nanoseconds()) / float64(totalTasks)))
+
+		items = append(items, worklog.Entry{
+			Client: worklog.IDNameField{
+				ID:   entry.Project.ClientID,
+				Name: entry.Project.ClientName,
+			},
+			Project: worklog.IDNameField{
+				ID:   entry.Project.ID,
+				Name: entry.Project.Name,
+			},
+			Task: worklog.IDNameField{
+				ID:   taskID,
+				Name: taskName,
+			},
+			Summary:            entry.Description,
+			Notes:              entry.Description,
+			Start:              entry.TimeInterval.Start,
+			BillableDuration:   splitBillableDuration,
+			UnbillableDuration: splitUnbillableDuration,
+		})
+	}
+
+	return &items, nil
+}
+
+func (c clockifyClient) FetchEntries(ctx context.Context, opts *client.FetchOpts) (*[]worklog.Entry, error) {
+	var items []worklog.Entry
+	currentPage := 1
+	pageSize := 100
+
+	// Naive pagination as the API does not return the number of total items
+	for currentPage*pageSize < MaxPageLength {
+		searchParams := &WorklogSearchParams{
+			Start:      opts.Start.Format(DateFormat),
+			End:        opts.End.Format(DateFormat),
+			Page:       currentPage,
+			PageSize:   pageSize,
+			Hydrated:   true,
+			InProgress: false,
+		}
+
+		searchURL, err := c.getSearchURL(opts.User, searchParams)
+		if err != nil {
+			return nil, err
+		}
+
+		resp, err := client.SendRequest(ctx, http.MethodGet, searchURL, nil, &c.opts.HTTPClientOptions)
+		if err != nil {
+			return nil, err
+		}
+
+		var entries []FetchEntry
+		if err = json.NewDecoder(resp.Body).Decode(&entries); err != nil {
+			return nil, err
+		}
+
+		// The API returned no entries, meaning no entries left
+		if len(entries) == 0 {
+			break
+		}
+
+		for _, entry := range entries {
+			billableDuration := entry.TimeInterval.End.Sub(entry.TimeInterval.Start)
+			unbillableDuration := time.Duration(0)
+
+			if !entry.Billable {
+				unbillableDuration = billableDuration
+				billableDuration = 0
+			}
+
+			if c.opts.TasksAsTags && len(entry.Tags) > 0 {
+				pageItems, err := c.splitEntry(entry, billableDuration, unbillableDuration)
+				if err != nil {
+					return nil, err
+				}
+
+				items = append(items, *pageItems...)
+			} else {
+				items = append(items, worklog.Entry{
+					Client: worklog.IDNameField{
+						ID:   entry.Project.ClientID,
+						Name: entry.Project.ClientName,
+					},
+					Project: worklog.IDNameField{
+						ID:   entry.Project.ID,
+						Name: entry.Project.Name,
+					},
+					Task: worklog.IDNameField{
+						ID:   entry.Task.ID,
+						Name: entry.Task.Name,
+					},
+					Summary:            entry.Task.Name,
+					Notes:              entry.Description,
+					Start:              entry.TimeInterval.Start,
+					BillableDuration:   billableDuration,
+					UnbillableDuration: unbillableDuration,
+				})
+			}
+		}
+
+		currentPage++
+	}
+
+	return &items, nil
+}
+
+// NewClient returns a new Clockify client.
+func NewClient(opts *ClientOpts) client.Fetcher {
+	return &clockifyClient{
+		opts: opts,
+	}
+}
---
+++ internal/pkg/client/clockify/clockify_test.go
@@ -0,0 +1,373 @@
+package clockify_test
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/gabor-boros/minutes/internal/pkg/client"
+	"github.com/gabor-boros/minutes/internal/pkg/client/clockify"
+	"github.com/gabor-boros/minutes/internal/pkg/worklog"
+	"github.com/stretchr/testify/require"
+)
+
+type mockServerOpts struct {
+	Path           string
+	Method         string
+	StatusCode     int
+	Token          string
+	TokenHeader    string
+	ResponseData   *[]clockify.FetchEntry
+	RemainingCalls *int
+}
+
+func mockServer(t *testing.T, e *mockServerOpts) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		require.Equal(t, e.Method, r.Method, "API call methods are not matching")
+		require.Equal(t, e.Path, r.URL.Path, "API call URLs are not matching")
+
+		if *e.RemainingCalls == 0 {
+			w.WriteHeader(http.StatusOK)
+			_ = json.NewEncoder(w).Encode(&[]clockify.FetchEntry{})
+			return
+		}
+
+		if e.Token != "" {
+			headerValue := r.Header.Get(e.TokenHeader)
+			require.Equal(t, e.Token, headerValue, "API call auth token mismatch")
+		}
+
+		if e.ResponseData != nil {
+			err := json.NewEncoder(w).Encode(e.ResponseData)
+			require.Nil(t, err, "cannot encode response data")
+		}
+
+		*e.RemainingCalls--
+		w.WriteHeader(e.StatusCode)
+	}))
+}
+
+func newMockServer(t *testing.T, opts *mockServerOpts) *httptest.Server {
+	mockServer := mockServer(t, opts)
+	require.NotNil(t, mockServer, "cannot create mock server")
+	return mockServer
+}
+
+func TestClockifyClient_FetchEntries(t *testing.T) {
+	start := time.Date(2021, 10, 2, 0, 0, 0, 0, time.Local)
+	end := time.Date(2021, 10, 2, 23, 59, 59, 0, time.Local)
+	remainingCalls := 1
+
+	expectedEntries := &[]worklog.Entry{
+		{
+			Client: worklog.IDNameField{
+				ID:   "456",
+				Name: "My Awesome Company",
+			},
+			Project: worklog.IDNameField{
+				ID:   "123",
+				Name: "MARVEL-101",
+			},
+			Task: worklog.IDNameField{
+				ID:   "789",
+				Name: "Meet with Iron Man",
+			},
+			Summary:            "Meet with Iron Man",
+			Notes:              "Have a coffee with Tony",
+			Start:              start,
+			BillableDuration:   end.Sub(start),
+			UnbillableDuration: 0,
+		},
+		{
+			Client: worklog.IDNameField{
+				ID:   "456",
+				Name: "My Awesome Company",
+			},
+			Project: worklog.IDNameField{
+				ID:   "123",
+				Name: "MARVEL-101",
+			},
+			Task: worklog.IDNameField{
+				ID:   "789",
+				Name: "Meet with Iron Man",
+			},
+			Summary:            "Meet with Iron Man",
+			Notes:              "Go back for my wallet",
+			Start:              start,
+			BillableDuration:   0,
+			UnbillableDuration: end.Sub(start),
+		},
+	}
+
+	mockServer := newMockServer(t, &mockServerOpts{
+		Path:           fmt.Sprintf(clockify.PathWorklog, "marvel-studios", "steve-rogers"),
+		Method:         http.MethodGet,
+		StatusCode:     http.StatusOK,
+		Token:          "t-o-k-e-n",
+		TokenHeader:    "X-Api-Key",
+		RemainingCalls: &remainingCalls,
+		ResponseData: &[]clockify.FetchEntry{
+			{
+				Description: "Have a coffee with Tony",
+				Billable:    true,
+				Project: clockify.Project{
+					ID:         "123",
+					Name:       "MARVEL-101",
+					ClientID:   "456",
+					ClientName: "My Awesome Company",
+				},
+				TimeInterval: clockify.Interval{
+					Start: start,
+					End:   end,
+				},
+				Task: clockify.Task{
+					ID:   "789",
+					Name: "Meet with Iron Man",
+				},
+				Tags: []clockify.Tag{
+					{
+						ID:   "1234",
+						Name: "Coffee",
+					},
+					{
+						ID:   "5678",
+						Name: "Meeting",
+					},
+					{
+						ID:   "9876",
+						Name: "TASK-1234",
+					},
+				},
+			},
+			{
+				Description: "Go back for my wallet",
+				Billable:    false,
+				Project: clockify.Project{
+					ID:         "123",
+					Name:       "MARVEL-101",
+					ClientID:   "456",
+					ClientName: "My Awesome Company",
+				},
+				TimeInterval: clockify.Interval{
+					Start: start,
+					End:   end,
+				},
+				Task: clockify.Task{
+					ID:   "789",
+					Name: "Meet with Iron Man",
+				},
+				Tags: []clockify.Tag{
+					{
+						ID:   "1234",
+						Name: "Coffee",
+					},
+					{
+						ID:   "5678",
+						Name: "Meeting",
+					},
+					{
+						ID:   "9876",
+						Name: "TASK-1234",
+					},
+					{
+						ID:   "5432",
+						Name: "TASK-5678",
+					},
+				},
+			},
+		},
+	})
+	defer mockServer.Close()
+
+	httpClientOpts := &client.HTTPClientOptions{
+		HTTPClient:  http.DefaultClient,
+		BaseURL:     mockServer.URL,
+		Token:       "t-o-k-e-n",
+		TokenHeader: "X-Api-Key",
+	}
+
+	clockifyClient := clockify.NewClient(&clockify.ClientOpts{
+		BaseClientOpts: client.BaseClientOpts{
+			HTTPClientOptions: *httpClientOpts,
+		},
+		Workspace: "marvel-studios",
+	})
+
+	entries, err := clockifyClient.FetchEntries(context.Background(), &client.FetchOpts{
+		User:  "steve-rogers",
+		Start: start,
+		End:   end,
+	})
+
+	require.Nil(t, err, "cannot fetch entries")
+	require.Equal(t, expectedEntries, entries, "fetched entries are not matching")
+}
+
+func TestClockifyClient_FetchEntries_TasksAsTags(t *testing.T) {
+	start := time.Date(2021, 10, 2, 0, 0, 0, 0, time.Local)
+	end := time.Date(2021, 10, 2, 23, 59, 59, 0, time.Local)
+	remainingCalls := 1
+
+	expectedEntries := &[]worklog.Entry{
+		{
+			Client: worklog.IDNameField{
+				ID:   "456",
+				Name: "My Awesome Company",
+			},
+			Project: worklog.IDNameField{
+				ID:   "123",
+				Name: "MARVEL-101",
+			},
+			Task: worklog.IDNameField{
+				ID:   "9876",
+				Name: "TASK-1234",
+			},
+			Summary:            "Have a coffee with Tony",
+			Notes:              "Have a coffee with Tony",
+			Start:              start,
+			BillableDuration:   end.Sub(start),
+			UnbillableDuration: 0,
+		},
+		{
+			Client: worklog.IDNameField{
+				ID:   "456",
+				Name: "My Awesome Company",
+			},
+			Project: worklog.IDNameField{
+				ID:   "123",
+				Name: "MARVEL-101",
+			},
+			Task: worklog.IDNameField{
+				ID:   "9876",
+				Name: "TASK-1234",
+			},
+			Summary:            "Go back for my wallet",
+			Notes:              "Go back for my wallet",
+			Start:              start,
+			BillableDuration:   0,
+			UnbillableDuration: end.Sub(start) / 2,
+		},
+		{
+			Client: worklog.IDNameField{
+				ID:   "456",
+				Name: "My Awesome Company",
+			},
+			Project: worklog.IDNameField{
+				ID:   "123",
+				Name: "MARVEL-101",
+			},
+			Task: worklog.IDNameField{
+				ID:   "5432",
+				Name: "TASK-5678",
+			},
+			Summary:            "Go back for my wallet",
+			Notes:              "Go back for my wallet",
+			Start:              start,
+			BillableDuration:   0,
+			UnbillableDuration: end.Sub(start) / 2,
+		},
+	}
+
+	mockServer := newMockServer(t, &mockServerOpts{
+		Path:           fmt.Sprintf(clockify.PathWorklog, "marvel-studios", "steve-rogers"),
+		Method:         http.MethodGet,
+		StatusCode:     http.StatusOK,
+		Token:          "t-o-k-e-n",
+		TokenHeader:    "X-Api-Key",
+		RemainingCalls: &remainingCalls,
+		ResponseData: &[]clockify.FetchEntry{
+			{
+				Description: "Have a coffee with Tony",
+				Billable:    true,
+				Project: clockify.Project{
+					ID:         "123",
+					Name:       "MARVEL-101",
+					ClientID:   "456",
+					ClientName: "My Awesome Company",
+				},
+				TimeInterval: clockify.Interval{
+					Start: start,
+					End:   end,
+				},
+				Task: clockify.Task{},
+				Tags: []clockify.Tag{
+					{
+						ID:   "1234",
+						Name: "Coffee",
+					},
+					{
+						ID:   "5678",
+						Name: "Meeting",
+					},
+					{
+						ID:   "9876",
+						Name: "TASK-1234",
+					},
+				},
+			},
+			{
+				Description: "Go back for my wallet",
+				Billable:    false,
+				Project: clockify.Project{
+					ID:         "123",
+					Name:       "MARVEL-101",
+					ClientID:   "456",
+					ClientName: "My Awesome Company",
+				},
+				TimeInterval: clockify.Interval{
+					Start: start,
+					End:   end,
+				},
+				Task: clockify.Task{},
+				Tags: []clockify.Tag{
+					{
+						ID:   "1234",
+						Name: "Coffee",
+					},
+					{
+						ID:   "5678",
+						Name: "Meeting",
+					},
+					{
+						ID:   "9876",
+						Name: "TASK-1234",
+					},
+					{
+						ID:   "5432",
+						Name: "TASK-5678",
+					},
+				},
+			},
+		},
+	})
+	defer mockServer.Close()
+
+	httpClientOpts := &client.HTTPClientOptions{
+		HTTPClient:  http.DefaultClient,
+		BaseURL:     mockServer.URL,
+		Token:       "t-o-k-e-n",
+		TokenHeader: "X-Api-Key",
+	}
+
+	clockifyClient := clockify.NewClient(&clockify.ClientOpts{
+		BaseClientOpts: client.BaseClientOpts{
+			HTTPClientOptions: *httpClientOpts,
+			TasksAsTags:       true,
+			TasksAsTagsRegex:  `^TASK\-\d+$`,
+		},
+		Workspace: "marvel-studios",
+	})
+
+	entries, err := clockifyClient.FetchEntries(context.Background(), &client.FetchOpts{
+		User:  "steve-rogers",
+		Start: start,
+		End:   end,
+	})
+
+	require.Nil(t, err, "cannot fetch entries")
+	require.Equal(t, expectedEntries, entries, "fetched entries are not matching")
+}
